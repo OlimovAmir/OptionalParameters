3b25bf75d951a27a273a6050a44f4823c9f3ed94
olimov Amir <olimov.amir@mail.ru>
2023-12-07 08:11:47 +0300

    Добавьте файлы проекта.

---
+++ OptionalParameters.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalParameters
+{
+    class OptionalParameters
+    {
+        static int Sum(int a, int b, bool enableLogging = false)
+        {
+            int result = a + b;
+            if (enableLogging)
+            {
+                Console.WriteLine("Value valiable a = " + a);
+                Console.WriteLine("Value valiable a = " + b);
+                Console.WriteLine("Value valiable a = " + result);
+            }
+
+            return result;
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
